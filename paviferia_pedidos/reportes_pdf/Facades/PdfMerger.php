86366e86cb46396fc633a35f737d1931fb4a4a21
Jean Pierre Julian Zavaleta <54478975+jjulianzavaleta@users.noreply.github.com>
2023-11-07 11:06:20 -0500

    Add files via upload

---
+++ paviferia_pedidos/reportes_pdf/Facades/PdfMerger.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PdfMerger extends Facade {
+	
+    protected static function getFacadeAccessor()
+    {
+        return 'pdf-merger';
+    }
+} 
